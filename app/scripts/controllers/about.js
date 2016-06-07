22c51f85df95487e1563b1dcce1f5a9c9dbd3965
McBlanco <caceresmarcelo89@gmail.com>
2016-06-07 19:26:43 -0300

    Creada la estructura del proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name facturationSystemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the facturationSystemApp
+ */
+angular.module('facturationSystemApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name facturationSystemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the facturationSystemApp
+ */
+angular.module('facturationSystemApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
